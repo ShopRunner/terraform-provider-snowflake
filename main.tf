b7cc641d74f86484443a3ac9bf731348395f7ec5
Dave Masino <davem@slalom.com>
2018-11-19 18:02:29 -0800

    Remove shoprunner references

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "snowflake_warehouse" "shoprunner_warehouse_terraform" {
-      name              =   "shoprunner_terraform"
+resource "snowflake_warehouse" "warehouse_terraform" {
+      name              =   "dev_wh"
       warehouse_size    =   "SMALL"
       auto_resume       =   false
       auto_suspend      =   600
-      comment           =   "shoprunner terraform development warehouse"
+      comment           =   "terraform development warehouse"
 }
 
-resource "snowflake_database" "shoprunner_database_terraform" {
-      name              =   "shoprunner_terraform_db"
-      comment           =   "shoprunner terraform development database"
+resource "snowflake_database" "database_terraform" {
+      name              =   "dev_db"
+      comment           =   "terraform development database"
 }
